0db18791c927a7146a8c20f72fa04241976bfe9a
buiduyhien <hien.bd@seesaa.vn>
2020-06-27 11:12:36 +0700

    change alpha image cover

--- Ultravisual/InspirationCell.swift
+++ Ultravisual/InspirationCell.swift
@@ -21,4 +21,32 @@ class InspirationCell: UICollectionViewCell {
     }
   }
   
+    override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
+        super.apply(layoutAttributes)
+        
+        let featuredHeight = UltravisualLayoutConstants.Cell.featuredHeight
+        let standarHeight = UltravisualLayoutConstants.Cell.standardHeight
+        
+        /*
+         Vì height của cell thay đổi nên frame.height thay đổi
+         standarHeight, featuredHeight is const
+         delta phụ thuộc vào frame.height
+         frame.height = {100, 280}
+         featuredHeight - frame.height = {180, 0}
+         featuredHeight - standarHeight = 180
+         (featuredHeight - frame.height) / (featuredHeight - standarHeight) = {1, 0}
+         delta = {0, 1}
+         */
+        let delta = 1 - ((featuredHeight - frame.height) / (featuredHeight - standarHeight))
+        
+        let minAlpha: CGFloat = 0.3
+        let maxAlpha: CGFloat = 0.75
+        
+        /*
+         maxAlpha - minAlpha = 0.45
+         delta * (maxAlpha - minAlpha) = { 0, 45 }
+         maxAlpha - (delta * (maxAlpha - minAlpha)) = {0.75, 0.3}
+         */
+        imageCoverView.alpha = maxAlpha - (delta * (maxAlpha - minAlpha))
+    }
 }
